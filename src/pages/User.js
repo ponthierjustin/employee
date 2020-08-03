e2d90ccb0331041682db98430a1fa7a401ac030d
Justin <ponthierjustin@gmail.com>
2020-08-03 17:53:57 -0400

    added sort by and filtering in work

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import API from "../utils/API";
-import UserCard from "../components/UserCard";
+import UserCard from "../components/UserCard.jsx";
 import Box from "@material-ui/core/Box";
+/* import NavBar from "../components/NavBar"; */
 
 class User extends Component {
   state = {
     users: [],
+    searchUsers: [],
+    search: ""
   };
 
   componentDidMount() {
@@ -23,12 +26,49 @@ class User extends Component {
       .catch((err) => console.log(err));
   };
 
+  sortUser = () => {
+    function compare(a, b) {
+      if (a.name.last > b.name.last) return 1;
+      if (b.name.last > a.name.last) return -1;
+
+      return 0;
+    }
+    const sortedUser = this.state.users.sort(compare);
+    this.setState({ users: sortedUser });
+  };
+
+  handleInputChange = (e) => {
+    e.preventDefault();
+    const employeeName = e.target.value;
+
+    const searchUsers = this.state.users.filter((employees) => {
+      const { first: firstName, last: lastName } = employees.name;
+
+      const searchedEmployeesName = `${firstName} ${lastName}`;
+      return searchedEmployeesName
+        .toLowerCase()
+        .includes(employeeName.toLowerCase().trim());
+    });
+
+    this.setState({
+      searchUsers: searchUsers,
+      search: employeeName,
+    });
+  };
+
   render() {
     return (
       <div>
         <div className="container">
           <Box display="flex" justifyContent="space-around">
             <h1 className="text-center">EMPLOYEES</h1>
+            <button className="text-center" onClick={this.sortUser}>sort</button>
+            <input
+              type="search"
+              name="search"
+              value={this.state.search}
+              onChange={this.handleInputChange}
+            />
           </Box>
           <Box
             display="flex"
@@ -47,7 +87,6 @@ class User extends Component {
                 email={user.email}
                 phone={user.phone}
                 dob={user.dob.date.slice(0, 10)}
-
               />
             ))}
           </Box>
